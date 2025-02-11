113bfd7ab697ab032292673ca87f03c4549620a2
Mostafa6894956 <mh5483610@gmail.com>
2025-02-11 20:26:02 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'azhar-institute-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/signup.html',
+  '/dashboard.html',
+  '/manifest.json',
+  'https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css',
+  'https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Cairo:wght@400;600&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then((response) => {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+}); 
